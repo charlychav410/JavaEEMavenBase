a160e1a1bafc91ac8627981753acd20812e397f2
charlychav410 <charlychav410@gmail.com>
2018-05-02 19:28:23 -0500

    javaEE

---
+++ scheduler-commons/src/main/java/mx/com/cinepolis/scheduler/commons/to/CatalogsTO.java
@@ -0,0 +1,32 @@
+package mx.com.cinepolis.scheduler.commons.to;
+
+import java.io.Serializable;
+
+public class CatalogsTO implements Serializable{
+	private int id;
+	private String name;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+}
